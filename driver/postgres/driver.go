da0e38dd0bb218a88fbf6f23181fc0e850c3f415
Brian W. Wolter <brian@brianwolter.com>
2016-12-21 17:17:00 -0500

    Database creation

--- driver/postgres/driver.go
+++ driver/postgres/driver.go
@@ -128,3 +128,35 @@ func (d *Driver) execVersionScript(t, s string, v int) error {
   return nil
 }
 
+/**
+ * Create a database, for debugging
+ */
+func createDatabase(u, n string) error {
+  
+  db, err := sql.Open("postgres", u)
+  if err != nil {
+    return err
+  }
+  
+  defer db.Close()
+  err = db.Ping()
+  if err != nil {
+    return err
+  }
+  
+  var exists int
+  err = db.QueryRow(fmt.Sprintf("SELECT 1 FROM pg_database WHERE datname = '%s'", n)).Scan(&exists)
+  if err != nil && err != sql.ErrNoRows {
+    return err
+  }
+  if exists == 1 {
+    return nil // ok
+  }
+  
+  _, err = db.Exec(fmt.Sprintf(`CREATE DATABASE "%s"`, n))
+  if err != nil {
+    return err
+  }
+  
+  return nil
+}
--- driver/postgres/driver_test.go
+++ driver/postgres/driver_test.go
@@ -7,12 +7,17 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
+const testDatabase = "go_upgrade_tests"
+
 func TestUpgrade(t *testing.T) {
   
-  d, err := New("postgres://postgres@localhost:5432/go_upgrade_tests?sslmode=disable")
+  err := createDatabase("postgres://postgres@localhost:5432/template1?sslmode=disable", testDatabase)
   if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
   
-  u, err := upgrade.New(upgrade.Config{Resources:"./test/versions/001", Driver:d})
+  d, err := New(fmt.Sprintf("postgres://postgres@localhost:5432/%s?sslmode=disable", testDatabase))
+  if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
+  
+  u, err := upgrade.New(upgrade.Config{Resources:"./test/postgres/001", Driver:d})
   if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
   
   n, err := u.Upgrade()
