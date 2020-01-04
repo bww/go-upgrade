a501bb13520a6a543b5acff67e89f9411cc449d4
Brian W. Wolter <brian@brianwolter.com>
2020-01-04 15:22:37 -0500

    Format

--- driver/postgres/driver_test.go
+++ driver/postgres/driver_test.go
@@ -1,60 +1,78 @@
 package postgres
 
 import (
-  "os"
-  "fmt"
-  "path"
-  "testing"
-  "github.com/stretchr/testify/assert"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
+	"testing"
 )
 
 import (
-  "github.com/bww/go-upgrade"
+	"github.com/bww/go-upgrade"
 )
 
 const (
-  testDatabase  = "go_upgrade_tests"
-  testURL       = "postgres://postgres@localhost:5432/template1?sslmode=disable"
+	testDatabase = "go_upgrade_tests"
+	testURL      = "postgres://postgres@localhost:5432/template1?sslmode=disable"
 )
 
 func TestUpgrade(t *testing.T) {
-  
-  t.Run("a", func(t *testing.T) {
-    var err error
-    var n int
-    
-    err = CreateDatabase(testURL, testDatabase)
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    
-    d, err := New(fmt.Sprintf("postgres://postgres@localhost:5432/%s?sslmode=disable", testDatabase))
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    defer d.Close()
-    
-    u, err := upgrade.New(upgrade.Config{Resources:path.Join(os.Getenv("GO_UPGRADE_TEST_RESOURCES"), "postgres/001"), Driver:d})
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    
-    n, err = u.Upgrade()
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    assert.Equal(t, 2, n)
-    
-    n, err = d.Version()
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    assert.Equal(t, 2, n)
-    
-    u, err = upgrade.New(upgrade.Config{Resources:path.Join(os.Getenv("GO_UPGRADE_TEST_RESOURCES"), "postgres/002"), Driver:d})
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    
-    n, err = u.Upgrade()
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    assert.Equal(t, 3, n)
-    
-    n, err = d.Version()
-    if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-    assert.Equal(t, 3, n)
-    
-  })
-  
-  err := DropDatabase(testURL, testDatabase)
-  if !assert.Nil(t, err, fmt.Sprintf("%v", err)) { return }
-  
+
+	t.Run("a", func(t *testing.T) {
+		var err error
+		var n int
+
+		err = CreateDatabase(testURL, testDatabase)
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+
+		d, err := New(fmt.Sprintf("postgres://postgres@localhost:5432/%s?sslmode=disable", testDatabase))
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+		defer d.Close()
+
+		u, err := upgrade.New(upgrade.Config{Resources: path.Join(os.Getenv("GO_UPGRADE_TEST_RESOURCES"), "postgres/001"), Driver: d})
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+
+		n, err = u.Upgrade()
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+		assert.Equal(t, 2, n)
+
+		n, err = d.Version()
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+		assert.Equal(t, 2, n)
+
+		u, err = upgrade.New(upgrade.Config{Resources: path.Join(os.Getenv("GO_UPGRADE_TEST_RESOURCES"), "postgres/002"), Driver: d})
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+
+		n, err = u.Upgrade()
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+		assert.Equal(t, 3, n)
+
+		n, err = d.Version()
+		if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+			return
+		}
+		assert.Equal(t, 3, n)
+
+	})
+
+	err := DropDatabase(testURL, testDatabase)
+	if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+		return
+	}
+
 }
--- version.go
+++ version.go
@@ -1,34 +1,35 @@
 package upgrade
 
 import (
-  "os"
-  "fmt"
-  "path"
-  "sort"
-  "strings"
-  "strconv"
-  "unicode"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 const (
-  upgrade   = "up"
-  rollback  = "down"
+	upgrade  = "up"
+	rollback = "down"
 )
 
 type Direction int
+
 const (
-  Upgrade   = Direction(iota)
-  Downgrade = Direction(iota)
+	Upgrade   = Direction(iota)
+	Downgrade = Direction(iota)
 )
 
 /**
  * An upgrade version
  */
 type Version struct {
-  Version   int
-  Upgrade   []byte
-  Rollback  []byte
+	Version  int
+	Upgrade  []byte
+	Rollback []byte
 }
 
 /**
@@ -37,104 +38,104 @@ type Version struct {
 type byVersion []*Version
 
 func (s byVersion) Len() int {
-  return len(s)
+	return len(s)
 }
 
 func (s byVersion) Swap(i, j int) {
-  s[i], s[j] = s[j], s[i]
+	s[i], s[j] = s[j], s[i]
 }
 
 func (s byVersion) Less(i, j int) bool {
-  return s[i].Version < s[j].Version
+	return s[i].Version < s[j].Version
 }
 
 /**
  * Load upgrade resources from a directory. An upgrade resource has
  * a filename of the following form:
- * 
+ *
  *  <version>_<up|down>[_<optional_description>]
  */
 func versionsFromResourcesAtPath(p string) ([]*Version, error) {
-  versions := make(map[int]*Version)
-  
-  dir, err := os.Open(p)
-  if err != nil {
-    return nil, err
-  }
-  
-  infos, err := dir.Readdir(0)
-  if err != nil {
-    return nil, err
-  }
-  
-  for _, e := range infos {
-    n := e.Name()
-    if len(n) > 0 {
-      if n[0] == '.' {
-        continue // skip dot files
-      }
-      if !unicode.IsDigit(rune(n[0])) {
-        continue // skip files that don't begin with a digit
-      }
-    }
-    
-    x := strings.IndexAny(n, "_-")
-    if x < 0 {
-      return nil, fmt.Errorf("Upgrade resource has invalid form: [%v] (version)", e.Name())
-    }
-    
-    v, err := strconv.Atoi(n[:x])
-    if err != nil {
-      return nil, fmt.Errorf("Upgrade resource has invalid form: invalid version number: [%v] %v", e.Name(), err)
-    }
-    if v < 1 {
-      return nil, fmt.Errorf("Version cannot be less than one: [%v] %v", e.Name(), v)
-    }
-    
-    n = n[x+1:]
-    
-    version, ok := versions[v]
-    if !ok {
-      version = &Version{Version:v}
-      versions[v] = version
-    }
-    
-    f, err := os.Open(path.Join(p, e.Name()))
-    if err != nil {
-      return nil, fmt.Errorf("Could not open resource: [%v] %v", e.Name(), err)
-    }
-    
-    data, err := ioutil.ReadAll(f)
-    if err != nil {
-      return nil, fmt.Errorf("Could not read resource: [%v] %v", e.Name(), err)
-    }
-    
-    switch {
-      case strings.HasPrefix(n, upgrade):
-        if version.Upgrade != nil {
-          return nil, fmt.Errorf("Upgrade resource redefined for version %v: [%v]", version.Version, e.Name())
-        }
-        version.Upgrade = data
-      case strings.HasPrefix(n, rollback):
-        if version.Rollback != nil {
-          return nil, fmt.Errorf("Rollback resource redefined for version %v: [%v]", version.Version, e.Name())
-        }
-        version.Rollback = data
-      default:
-        return nil, fmt.Errorf("Upgrade resource has invalid form: invalid resource type [%v] %v", e.Name(), v)
-    }
-    
-  }
-  
-  i, out := 0, make([]*Version, len(versions))
-  for _, e := range versions {
-    if e.Upgrade == nil {
-      return nil, fmt.Errorf("Version %v is missing an upgrade resource", e.Version)
-    }
-    out[i] = e
-    i++
-  }
-  
-  sort.Sort(byVersion(out))
-  return out, nil
+	versions := make(map[int]*Version)
+
+	dir, err := os.Open(p)
+	if err != nil {
+		return nil, err
+	}
+
+	infos, err := dir.Readdir(0)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, e := range infos {
+		n := e.Name()
+		if len(n) > 0 {
+			if n[0] == '.' {
+				continue // skip dot files
+			}
+			if !unicode.IsDigit(rune(n[0])) {
+				continue // skip files that don't begin with a digit
+			}
+		}
+
+		x := strings.IndexAny(n, "_-")
+		if x < 0 {
+			return nil, fmt.Errorf("Upgrade resource has invalid form: [%v] (version)", e.Name())
+		}
+
+		v, err := strconv.Atoi(n[:x])
+		if err != nil {
+			return nil, fmt.Errorf("Upgrade resource has invalid form: invalid version number: [%v] %v", e.Name(), err)
+		}
+		if v < 1 {
+			return nil, fmt.Errorf("Version cannot be less than one: [%v] %v", e.Name(), v)
+		}
+
+		n = n[x+1:]
+
+		version, ok := versions[v]
+		if !ok {
+			version = &Version{Version: v}
+			versions[v] = version
+		}
+
+		f, err := os.Open(path.Join(p, e.Name()))
+		if err != nil {
+			return nil, fmt.Errorf("Could not open resource: [%v] %v", e.Name(), err)
+		}
+
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			return nil, fmt.Errorf("Could not read resource: [%v] %v", e.Name(), err)
+		}
+
+		switch {
+		case strings.HasPrefix(n, upgrade):
+			if version.Upgrade != nil {
+				return nil, fmt.Errorf("Upgrade resource redefined for version %v: [%v]", version.Version, e.Name())
+			}
+			version.Upgrade = data
+		case strings.HasPrefix(n, rollback):
+			if version.Rollback != nil {
+				return nil, fmt.Errorf("Rollback resource redefined for version %v: [%v]", version.Version, e.Name())
+			}
+			version.Rollback = data
+		default:
+			return nil, fmt.Errorf("Upgrade resource has invalid form: invalid resource type [%v] %v", e.Name(), v)
+		}
+
+	}
+
+	i, out := 0, make([]*Version, len(versions))
+	for _, e := range versions {
+		if e.Upgrade == nil {
+			return nil, fmt.Errorf("Version %v is missing an upgrade resource", e.Version)
+		}
+		out[i] = e
+		i++
+	}
+
+	sort.Sort(byVersion(out))
+	return out, nil
 }
