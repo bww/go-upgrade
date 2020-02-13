c6bd7d3527ec508ef562024c5a13cd6372876109
Brian W. Wolter <brian@brianwolter.com>
2020-02-13 17:58:29 -0500

    ...

--- v1/upgrade_test.go
+++ v1/upgrade_test.go
@@ -24,12 +24,8 @@ func (d *testDriver) Upgrade(v Version) error {
 
 func TestValidVersions(t *testing.T) {
 	u, err := New(Config{Resources: path.Join(os.Getenv("GO_UPGRADE_TEST_RESOURCES"), "versions/001"), Driver: &testDriver{0}})
-	if !assert.Nil(t, err, fmt.Sprintf("%v", err)) {
-		return
-	}
-	if !assert.Len(t, u.versions, 3) {
-		return
-	}
+	assert.Nil(t, err, fmt.Sprintf("%v", err))
+	assert.Len(t, u.versions, 3)
 
 	assert.Equal(t, 1, u.versions[0].Version)
 	assert.Equal(t, []byte("1. Up"), u.versions[0].Upgrade)
@@ -42,8 +38,6 @@ func TestValidVersions(t *testing.T) {
 	assert.Equal(t, 4, u.versions[2].Version) // this is weird, but valid; versions can be sparse
 	assert.Equal(t, []byte("4. Up"), u.versions[2].Upgrade)
 	assert.Equal(t, []byte("4. Down"), u.versions[2].Rollback)
-
-	fmt.Println(u)
 }
 
 func TestMalformedVersions(t *testing.T) {
